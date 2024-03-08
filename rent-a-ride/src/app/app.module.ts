789ace423cd685eff406f6c5776df0b43fbfbb1f
Shruti Shrivastav <shrutishrivastav938@gmail.com>
2024-03-08 11:18:47 +0530

    updated all

--- rent-a-ride/src/app/app.module.ts
+++ rent-a-ride/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, SignupComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideClientHydration()],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
