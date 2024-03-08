f1c9b76d3384d59faedb4df79395532707ecacae
deshmukharya <arya.deshmukh@innovapptive.com>
2024-03-08 17:30:11 +0530

    components added

--- rent-a-ride/src/app/app.module.ts
+++ rent-a-ride/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { SearchComponent } from './search/search.component';
+import { CarlistingComponent } from './carlisting/carlisting.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent,
+    SearchComponent,
+    CarlistingComponent,
+    CityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     provideClientHydration()
