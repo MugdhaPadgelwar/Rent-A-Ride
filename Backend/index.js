536ba0a3182c547518bd789fa24999bf5b049032
Sagar <sagar.arora198@gmail.com>
2024-03-12 10:15:46 +0530

    swagger created

--- Backend/index.js
+++ Backend/index.js
@@ -11,11 +11,18 @@ require("./database/connection");
 // Load environment variables from .env file
 require("dotenv").config();
 
+// swaggerjsDocs import for swagger defination
+const swaggerJSDoc = require('swagger-jsdoc');
+
+// swaggerUi
+const swaggerUi = require('swagger-ui-express');
+
 // Set the port to listen for incoming requests
 const port = process.env.SERVER_PORT;
 
 // Middleware for parsing JSON bodies
 app.use(express.json());
+
 
 // Middleware for parsing URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,6 +39,34 @@ const adminRoutes = require("./router/adminRouter");
 const carRoutes = require("./router/carRouter");
 const citiesRoutes = require("./router/citiesRouter");
 
+
+// define swaggerDefination
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Rent-Ride Express APIs',
+    version: '1.0.0',
+  },
+  servers:[
+    {
+      url:'http://localhost:3001',
+      description:"development server"
+    }
+  ],
+
+};
+// options for swagger 
+const options = {
+  swaggerDefinition,
+
+  // Paths to files containing OpenAPI definitions
+  apis: ['./router/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(options);
+// middleware for swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 // Mount route handlers
 app.use("/users", userRoutes);
 app.use("/ratings", ratingRoutes);
@@ -40,6 +75,8 @@ app.use("/locations", locationRoutes);
 app.use("/admin", adminRoutes);
 app.use("/cars", carRoutes);
 app.use("/cities", citiesRoutes);
+
+
 
 // Start the server and listen for incoming requests
 app.listen(port, () => {
